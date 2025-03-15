ed0bbd564fcd4b1ebe4f6dda43728fc6c62a436b
Don Hamilton III <donhamiltoniii@gmail.com>
2025-03-15 16:03:55 -0400

    fix: fix typo in docker-install

--- shell/install-docker.sh
+++ shell/install-docker.sh
@@ -1,4 +1,4 @@
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; don
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Add Docker's official GPG key:
 sudo apt-get update
